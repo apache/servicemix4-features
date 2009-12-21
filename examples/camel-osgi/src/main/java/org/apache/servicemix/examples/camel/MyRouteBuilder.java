68c8bdbbe8391bc88d4a614050b9658852de58d8
Gert Vanthienen <gertv@apache.org>
2009-12-21 20:09:11 +0000

    SMX4-451: camel-osgi demo fails with org.apache.camel.FailedToStartRouteException
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/features/trunk@892956 13f79535-47bb-0310-9956-ffa450edef68

--- examples/camel-osgi/src/main/java/org/apache/servicemix/examples/camel/MyRouteBuilder.java
+++ examples/camel-osgi/src/main/java/org/apache/servicemix/examples/camel/MyRouteBuilder.java
@@ -39,7 +39,7 @@ public class MyRouteBuilder extends RouteBuilder {
         MyTransform transform = new MyTransform();
         transform.setPrefix("JavaDSL");
         
-        from("timer://myTimer?fixedRate=true&period=2000")
+        from("timer://javaTimer?fixedRate=true&period=2000")
             .bean(transform, "transform")
             .to("log:ExampleRouter");        
     }
