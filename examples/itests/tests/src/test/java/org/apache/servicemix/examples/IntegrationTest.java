a90394e5d62b1a3270664d4e4406d56201ccc304
Freeman Yue Fang <ffang@apache.org>
2010-05-07 03:36:31 +0000

    [SMX4-415|SMX4-455]add necessary bundles for activemq 5.3.2 and fix test failure
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/features/trunk@941991 13f79535-47bb-0310-9956-ffa450edef68

--- examples/itests/tests/src/test/java/org/apache/servicemix/examples/IntegrationTest.java
+++ examples/itests/tests/src/test/java/org/apache/servicemix/examples/IntegrationTest.java
@@ -88,7 +88,11 @@ public class IntegrationTest extends AbstractIntegrationTest {
             getBundle("org.apache.geronimo.specs", "geronimo-jta_1.1_spec"),
                        
             //for activemq
-            
+            getBundle("commons-lang", "commons-lang"),
+            getBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.josql"),
+            getBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.oro"),
+            getBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.velocity"),
+            getBundle("commons-collections", "commons-collections"),
             getBundle("org.springframework", "spring-jms"),
             getBundle("org.springframework", "spring-tx"),
             getBundle("org.apache.geronimo.specs", "geronimo-j2ee-management_1.1_spec"),
