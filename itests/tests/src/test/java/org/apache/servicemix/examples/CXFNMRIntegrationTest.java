621d6579e093657745f6b8d122fc1e2a8f9f97f5
Freeman Yue Fang <ffang@apache.org>
2012-10-16 02:27:44 +0000

    [SMX4-1263]upgrade to activemq 5.7.0
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/features/trunk@1398618 13f79535-47bb-0310-9956-ffa450edef68

--- itests/tests/src/test/java/org/apache/servicemix/examples/CXFNMRIntegrationTest.java
+++ itests/tests/src/test/java/org/apache/servicemix/examples/CXFNMRIntegrationTest.java
@@ -75,7 +75,7 @@ public class CXFNMRIntegrationTest extends AbstractIntegrationTest {
             getBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.jaxb-impl"),
             getBundle("org.apache.neethi", "neethi"),
             getBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.saaj-impl"),
-            getBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.commons-pool"),
+            getBundle("commons-pool", "commons-pool"),
             getBundle("org.codehaus.woodstox", "stax2-api"),
             getBundle("org.codehaus.woodstox", "woodstox-core-asl"),
             getBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.dom4j"),
